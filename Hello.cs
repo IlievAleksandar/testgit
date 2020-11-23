2ed4c987ca7b339af42c998d1bfbc4a4d48912a8
IlievAleksandar <74927504+IlievAleksandar@users.noreply.github.com>
2020-11-23 21:01:03 +0100

    Update Hello.cs

--- Hello.cs
+++ Hello.cs
@@ -6,3 +6,40 @@ public class Hello
       System.Console.WriteLine("Hello, World!");
    }
 }
+// Ime datoteke: Hello.cs
+public class Hello
+{
+   public static void Main()
+   {
+	System.Console.WriteLine("Hello, World!");
+	System.Console.WriteLine("Hello, GitHub!");
+   }
+// Ime datoteke: Hello.cs
+public class Hello
+{
+   public static void Main()
+   {
+	System.Console.WriteLine("Hello, World!");
+	System.Console.WriteLine("Hello, Local!");
+   }
+}
+// Ime datoteke: Hello.cs
+public class Hello
+{
+   public static void Main()
+   {
+	System.Console.WriteLine("Hello, World!");
+	System.Console.WriteLine("Hello, GitHub!");
+	System.Console.WriteLine("Hello, Local!");
+   }
+}
+   // Ime datoteke: Hello.cs
+public class Hello
+{
+   public static void Main()
+   {
+	System.Console.WriteLine("Hello, World!");
+	System.Console.WriteLine("Hello, GitHub!");
+	System.Console.WriteLine("Hello, Branch!");
+   }
+}
